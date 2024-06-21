6c140f8b0663220ef5b551eaa9227e05775f54d2
king-of-coding <165642803+king-of-coding@users.noreply.github.com>
2024-06-21 12:53:04 +0900

    Update scripts.js
    함수 관련 주석 추가

--- silverchef/static/js/scripts.js
+++ silverchef/static/js/scripts.js
@@ -1,9 +1,9 @@
-document.getElementById('search-button').addEventListener('click', async () => {
-    const recipeInput = document.getElementById("recipe-input").value;
+document.getElementById('search-button').addEventListener('click', async () => { // 버튼을 누르면 함수 실행
+    const recipeInput = document.getElementById("recipe-input").value; // 사용자가 입력한 레시피 저장
 
     if (recipeInput) {
         try {
-            const response = await fetch(`/translate_and_search?recipe_name_ko=${encodeURIComponent(recipeInput)}`);
+            const response = await fetch(`/translate_and_search?recipe_name_ko=${encodeURIComponent(recipeInput)}`);// fetch api를 사용하여 서버에 요청 보냄
             const data = await response.json();
             displayRecipes(data.recipes);
         } catch (error) {
@@ -12,7 +12,7 @@ document.getElementById('search-button').addEventListener('click', async () => {
     }
 });
 
-function displayRecipes(recipes) {
+function displayRecipes(recipes) { // 레시피 목록을 화면에 표시하는 함수
     const recipesContainer = document.getElementById("recipes");
     recipesContainer.innerHTML = "";
 
