2f6f2fcc2c058ac661b29b2fd2ec5dca40ec6356
chimny04 <144990670+chimny04@users.noreply.github.com>
2024-06-21 12:02:45 +0900

    Update app.py
    주석 추가

--- silverchef/app.py
+++ silverchef/app.py
@@ -3,9 +3,11 @@ from flask import Flask, request, jsonify, render_template, url_for
 from googletrans import Translator
 import requests
 
+# flask 어플리케이션 생성
 app = Flask(__name__)
 translator = Translator()
 
+# '/' 경로로 접근시 호출되는 함수, html 템플릿을 렌더링함
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,15 +16,18 @@ def home():
 def translate_and_search_api():
     recipe_name_ko = request.args.get('recipe_name_ko')
     if recipe_name_ko:
+        # 한국어로 입력받은 레시피 이름을 영어로 번역함
         recipe_name_en = translator.translate(recipe_name_ko, src='ko', dest='en').text
         return jsonify(get_top_5_recipes(recipe_name_en))
     return jsonify({'recipes': []})
 
+# 주어진 레시피 이름으로 상위 5개의 레시피를 검색하는 함수
 def get_top_5_recipes(recipe_name):
     api_url = "https://api.edamam.com/search"
-    app_id = "3fb51f64"
-    app_key = "95d50643632f0c49d6d2ab3d5a2068d2"
+    app_id = "enter your app id"
+    app_key = "enter your app key"
 
+    # api 요청에 필요한 변수 설정
     params = {
         "q": recipe_name,
         "app_id": app_id,
@@ -31,14 +36,16 @@ def get_top_5_recipes(recipe_name):
         "to": 5,
     }
 
+    # api에 get 요청
     response = requests.get(api_url, params=params)
     data = response.json()
 
+    # 검색 결과에서 레시피 정보 추출
     recipes = []
     if "hits" in data and data["hits"]:
         for hit in data["hits"]:
             recipe = hit["recipe"]
-            # Translate the recipe label to Korean
+            # 레시피 이름 한국어로 번역
             recipe_label_ko = translator.translate(recipe["label"], src='en', dest='ko').text
             recipes.append({
                 "label": recipe_label_ko,
@@ -47,6 +54,7 @@ def get_top_5_recipes(recipe_name):
             })
     return {"recipes": recipes}
 
+# 어플리케이션 시작
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=5000)
